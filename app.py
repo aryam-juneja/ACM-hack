41cc62cd176a574bbdb786d45b38bfff0806ad97
Beelzebub <49526278+Aryam14@users.noreply.github.com>
2023-04-09 07:10:09 +0530

    flask code for store

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Drink more coffee !!"
+@app.route('/')
+def homepage():
+    products = get_products()  
+    return render_template('homepage.html', products=products)
 
-app.run(host="0.0.0.0", port=80)
+@app.route('/product/<int:product_id>')
+def product_detail(product_id):
+    product = get_product_by_id(product_id) 
+    return render_template('product_detail.html', product=product)
+
+\
+@app.route('/cart')
+def cart():
+    cart_items = get_cart_items()  
+    return render_template('cart.html', cart_items=cart_items)
+
+
+@app.route('/checkout', methods=['POST'])
+def checkout():
+    
+    return render_template('checkout.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
